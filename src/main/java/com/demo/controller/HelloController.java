1caf8c53013b795d1ae53b2d289a74aef5b6f1e9
Pratik Kanani <pratik.kanani30@gmail.com>
2020-10-13 19:20:19 +0530

    Update HelloController.java
    webhook test

--- src/main/java/com/demo/controller/HelloController.java
+++ src/main/java/com/demo/controller/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "api/greeting", produces = "application/json")
+// controller to get greeting
 public class HelloController {
 
     @GetMapping()
